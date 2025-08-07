3c71627b0d2e0d2c7be2fd84d68079eeeb2af877
raihannafi712 <shitabraihan712@gmail.com>
2025-08-08 02:08:11 +0600

    h

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import Container from './Container';
 
 const About = () => {
   return (
-    <section className="bg-black py-[1000px] z-[30] ">
+    <section className="bg-black sticky py-[100px] z-[70] ">
       <Container>
         <h3 className="font-rob text-[80px] text-white font-black leading-5  ">
           About me
